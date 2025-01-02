077988502474a52b6dcad1e17a5fe639afe88347
anhphongtrng <22521092@gm.uit.edu.vn>
2025-01-03 01:17:33 +0700

    Add RegisterSreen and link Login with Register

---
+++ app/screens/RegisterScreen.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react'
+import { View, StyleSheet } from 'react-native'
+import { Input, Button } from 'react-native-elements';
+
+const RegisterScreen = () => {
+    const [email, setEmail] = useState('');
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
+    const [imageURl, setImageUrl] = useState('');
+    return (
+        <View style={styles.container}>
+            <Input
+                placeholder="Enter your name"
+                label="Name"
+                leftIcon={{ type: 'material', name: 'badge' }}
+                value={name}
+                onChangeText={text => setName(text)}
+            />
+            <Input
+                placeholder="Enter your email"
+                label="Email"
+                leftIcon={{ type: 'material', name: 'email' }}
+                value={email}
+                onChangeText={text => setEmail(text)}
+            />
+            <Input
+                placeholder="Enter your password"
+                label="Password"
+                leftIcon={{ type: 'material', name: 'lock' }}
+                value={password}
+                onChangeText={text => setPassword(text)}
+                secureTextEntry
+            />
+            <Input
+                placeholder="Enter your image Url"
+                label="Profile Picture"
+                leftIcon={{ type: 'material', name: 'face' }}
+                value={imageURl}
+                onChangeText={text => setImageUrl(text)}
+            />
+
+            <Button title="Register" style={styles.button} />
+        </View>
+    )
+}
+
+export default RegisterScreen
+const styles = StyleSheet.create({
+    button: {
+        width: 200,
+        marginTop: 10
+    },
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        padding: 10
+    }
+})
